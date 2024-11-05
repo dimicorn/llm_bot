8f779b81922d27163c8578426fded50df370361d
dimicorn <zagorulia.ds@phystech.edu>
2024-11-05 12:12:56 +0300

    secrets

--- main.py
+++ main.py
@@ -7,19 +7,25 @@ from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from llm import LLM
-import os
+import streamlit as st
+# import os
 
+# Everything is accessible via the st.secrets dict:
 
-with open(f'{os.path.expanduser('~')}/.streamlit/config.toml', 'rb') as f:
-    cfg = load(f)
+# st.write("DB username:", st.secrets["db_username"])
+# st.write("DB password:", st.secrets["db_password"])
 
-document_loader = PyPDFDirectoryLoader(cfg['data_path'])
+# print(st.secrets)
+# with open(f'{os.path.expanduser('~')}/.streamlit/config.toml', 'rb') as f:
+#     cfg = load(f)
+
+document_loader = PyPDFDirectoryLoader(st.secrets['data_path'])
 textsplitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=20)
 raw_data = document_loader.load_and_split(textsplitter)
 embeddings = OpenAIEmbeddings()
 vector_store = InMemoryVectorStore.from_documents(raw_data, embeddings)
 
-bot = AsyncTeleBot(cfg['tg_bot'])
+bot = AsyncTeleBot(st.secrets['tg_bot'])
 
 @bot.message_handler(commands=['start'])
 async def send_welcome(message):
@@ -27,7 +33,7 @@ async def send_welcome(message):
 
 @bot.message_handler()
 async def send_text(message):
-    client = OpenAI(api_key=cfg['open_ai'])
+    client = OpenAI(api_key=st.secrets['open_ai'])
     llm = LLM(client, vector_store)
     answer = llm.rag_search(message.text, pprint=True)
     await bot.send_message(message.chat.id, answer)
